3947d90825bdae96370a766421a743eda770b42b
yairJCT <irigo1@gmail.com>
2017-01-10 12:04:56 +0200

    Data grid

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BE;
 using BL;
+using System.Collections.ObjectModel;
 
 namespace PL
 {
@@ -22,16 +23,48 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        MyBL bl;
+        private ObservableCollection<Speciality> _SpecialityCollection;
+        private ObservableCollection<Worker> _WorkersCollection;
+
         public MainWindow()
         {
             InitializeComponent();
+            bl = MyBL.Instance;
+            comboBoxDiscpline.DataContext = Enum.GetNames(typeof(Enums.Discipline)).ToList();
+           _SpecialityCollection = new ObservableCollection<Speciality>(bl.getSpecsList());
+            dgSpecialities.DataContext = _SpecialityCollection;
+
+            _WorkersCollection = new ObservableCollection<Worker>(bl.getWorkersList());
+            lbWorkers.DataContext = _WorkersCollection;
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            Speciality sp = new Speciality(Enums.Discipline.General,tBname.Text,Convert.ToInt32(tBminRate.Text), Convert.ToInt32(tBmaxRate.Text));
-            MyBL bl = MyBL.Instance;
+            Enums.Discipline myDiscipline;
+            Enum.TryParse(comboBoxDiscpline.Text, out myDiscipline);
+            Speciality sp = new Speciality(myDiscipline, tBname.Text,Convert.ToInt32(tBminRate.Text), Convert.ToInt32(tBmaxRate.Text));
             bl.addSpec(sp);
+            _SpecialityCollection.Add(sp);
         }
+
+        private void addWorkerbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Worker wo = new Worker(tBWorkerID.Text,tBWorkerFirstName.Text,tBWorkerLastname.Text, dpWorkerBirthday.SelectedDate.Value.Date);
+            bl.addWorker(wo);
+            _WorkersCollection.Add(wo);
+
+            
+        }
+
+        private void addEmployersbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Employer emp = new Employer(int.Parse(tBEmployersID.Text),tBEmployersName.Text);
+            bl.addEmployer(emp);
+        }
+
+       
     }
 }
